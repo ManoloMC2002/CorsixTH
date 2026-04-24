53ab7e224a0d22e1a13ad234a29fca4ce4ca7ed0
Manolo Manseau Cote <manolo.m.cote@gmail.com>
2026-04-24 11:35:27 -0400

    Ajoute tests pour incrementation earthquake et configuration de luaunit

---
+++ tests/test_base.lua
@@ -0,0 +1,35 @@
+﻿package.path = package.path
+  .. ";./?.lua"
+  .. ";../?.lua"
+  .. ";./CorsixTH/?.lua"
+  .. ";./CorsixTH/Lua/?.lua"
+  .. ";./CorsixTH/Lua/?/init.lua"
+  .. ";./CorsixTH/Lua/?/?.lua"
+
+local TestBase = {}
+
+function TestBase.setup()
+  package.preload["TH"] = function()
+    return {}
+  end
+
+  _G.Object = {}
+  _G.Object.__index = _G.Object
+
+  _G.class = function(name)
+    return function(base)
+      local cls = {}
+      cls.__index = cls
+      cls.__name = name
+
+      if base then
+        setmetatable(cls, { __index = base })
+      end
+
+      _G[name] = cls
+      return cls
+    end
+  end
+end
+
+return TestBase
---
+++ tests/test_machine_earthquake.lua
@@ -0,0 +1,65 @@
+﻿local lu = require("luaunit")
+
+local TestBase = require("tests.test_base")
+TestBase.setup()
+
+require("CorsixTH.Lua.entities.machine")
+
+local Machine = _G["Machine"]
+
+TestMachineEarthquake = {}
+
+-- Vérifie qu’un tremblement de terre n’incrémente pas total_usage
+-- mais incrémente bien times_used
+function TestMachineEarthquake:testEarthquakeDoesNotIncrementTotalUsage()
+  local fake_machine = setmetatable({}, { __index = Machine })
+
+  fake_machine.times_used = 0
+  fake_machine.total_usage = 0
+
+  fake_machine:incrementUsageCounts(false, true)
+
+  lu.assertEquals(fake_machine.times_used, 1)
+  lu.assertEquals(fake_machine.total_usage, 0)
+end
+
+-- Vérifie qu’une utilisation normale (pas tremblement de terre) incrémente tous les compteurs
+function TestMachineEarthquake:testNormalUsageIncrementsTotalUsage()
+  local fake_machine = setmetatable({}, { __index = Machine })
+
+  fake_machine.times_used = 0
+  fake_machine.total_usage = 0
+
+  fake_machine:incrementUsageCounts(false, false)
+
+  lu.assertEquals(fake_machine.times_used, 1)
+  lu.assertEquals(fake_machine.total_usage, 1)
+end
+
+-- Vérifie que total_usage_only n’incrémente pas times_used mais incrémente total_usage
+function TestMachineEarthquake:testTotalUsageOnlyDoesNotIncrementTimesUsed()
+  local fake_machine = setmetatable({}, { __index = Machine })
+
+  fake_machine.times_used = 0
+  fake_machine.total_usage = 0
+
+  fake_machine:incrementUsageCounts(true, false)
+
+  lu.assertEquals(fake_machine.times_used, 0)
+  lu.assertEquals(fake_machine.total_usage, 1)
+end
+
+-- Vérifie qu’un tremblement de terre incrémente times_used sans modifier total_usage
+function TestMachineEarthquake:testEarthquakeKeepsExistingTotalUsage()
+  local fake_machine = setmetatable({}, { __index = Machine })
+
+  fake_machine.times_used = 5
+  fake_machine.total_usage = 10
+
+  fake_machine:incrementUsageCounts(false, true)
+
+  lu.assertEquals(fake_machine.times_used, 6)
+  lu.assertEquals(fake_machine.total_usage, 10)
+end
+
+os.exit(lu.LuaUnit.run())
